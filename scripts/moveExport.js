c42397b9736046a92b058d83a3350a7466b6bd6c
skarab42 <skarab@onlfait.ch>
2021-01-06 10:55:40 +0100

    moved firebase hosting at root level

--- scripts/moveExport.js
+++ scripts/moveExport.js
@@ -2,10 +2,11 @@ const fs = require("fs-extra");
 const path = require("path");
 
 const src = path.resolve(__dirname, "../__sapper__/export");
-const dest = path.resolve(__dirname, "../docs/export");
+const dest = path.resolve(__dirname, "../export");
 
 try {
   console.log("Moving export directory...");
+  fs.ensureDirSync(dest);
   fs.moveSync(src, dest, { overwrite: true });
 } catch (error) {
   console.error("ERROR >>>", error.message);
