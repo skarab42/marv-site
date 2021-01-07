88f771590b4d87ccb120c191578f2c8af25e410b
skarab42 <skarab@onlfait.ch>
2021-01-07 07:33:05 +0100

    improved docs readablity

--- src/routes/[lang]/docs/[page].svelte
+++ src/routes/[lang]/docs/[page].svelte
@@ -32,17 +32,40 @@
 <script>
   export let post;
   export let menu;
+
+  const prose = "prose-sm sm:prose lg:prose-lg xl:prose-xl";
 </script>
+
+<style>
+  :global(.docs-menu p > a) {
+    font-size: 1.2rem;
+    text-transform: uppercase;
+  }
+
+  :global(.docs-menu a) {
+    font-size: 1rem;
+    font-weight: 500;
+    color: rgb(211, 219, 224);
+    padding: 0.4rem 0.8rem;
+    display: inline-block;
+  }
+
+  :global(.docs-menu a:hover) {
+    color: rgb(226, 64, 218);
+  }
+</style>
 
 <svelte:head>
   <title>{post.title} - Docs - {$fingerprint}</title>
 </svelte:head>
 
 <div class="flex">
-  <div class="docs-menu flex flex-col bg-gray-700" style="min-width: 250px">
-    {@html menu}
+  <div class="bg-gray-700" style="min-width: 250px">
+    <div class="docs-menu flex flex-col sticky top-0">
+      {@html menu}
+    </div>
   </div>
-  <div class="prose p-5 flex-auto max-w-full bg-gray-400">
+  <div class="{prose} p-5 flex-auto max-w-full text-gray-800 bg-gray-400">
     {@html post.html}
   </div>
 </div>
